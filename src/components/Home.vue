fbb030c406130fc6b1e3ca00519cfa09dd0dd5e7
hmq201211 <879602728@qq.com>
2019-08-08 17:18:16 +0800

    190808

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="hello">
-        <h3>{{$store.state.count}}</h3>
+        <h3>{{$store.state.count}}</h3>//这里用到了vuex, 组件间通讯<br><br>
         <h1>Top Component</h1>
-        <router-link to="/demo-interpolation-expression" @click.native="nativeMethod">interpolation expression demo</router-link><br><br>
+        <router-link to="/demo-interpolation-expression" @click.native="nativeMethod">interpolation expression demo</router-link>// 这里.native 用在vue或者自定义组件上时可以获取原生事件的响应<br><br>
         <router-link to="/demo-watch-methods-computed">methods computed and watch demo</router-link><br><br>
         <router-link to="/demo-props">props demo</router-link><br><br>
         <router-link to="/demo-emit-and-listen">emit and listen demo</router-link><br><br>
         <router-link to="/demo-class-and-style">class and style demo</router-link><br><br>
-        <router-link to="/demo-element-list">element ui demo</router-link><br><br>
+        <router-link to="/demo-element-list">element ui demo</router-link>//.stop 可以用来阻止事件的扩散, .prevent 可以截取事件的原生处理方法<br><br>
         <router-link to="/demo-vuex">vuex demo</router-link><br><br>
         <router-link to="/demo-slot">slot demo</router-link><br><br>
         ---------------------------------------------------------------------------------------------------
--- src/components/demo-emit-and-listen/demo-listener.vue
+++ src/components/demo-emit-and-listen/demo-listener.vue
@@ -1,5 +1,8 @@
 <template>
     <div>
+        // 单向数据流, 只能上级组件传递数据给下级组件,如果下级组件想要返回消息给上级组件
+        必须建立emit-listen , 下级组件发送消息, 上级组件监听消息.<br><br>
+        //以下的7种数组的处理方法会被vue的响应式依赖侦察到, 如果影响到了对应的dom, 那么vue就会更新dom来更新视图<br><br>
         <DemoSender v-for="(item,index) in listened_array_operations" :index="index" :msg="item"
                     v-bind:key="index" @delete="gotEmittedMessage"></DemoSender>
     </div>
